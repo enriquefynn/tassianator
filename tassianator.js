1ae6e9d74b40a2c4bfebe5158e26128a860efe4d
Leonardo Alt <leonardoaltt@gmail.com>
2015-06-15 22:46:44 +0200

    asd

--- tassianator.js
+++ tassianator.js
@@ -2,6 +2,19 @@
 var App = angular.module('App', []);
 App.controller('tassianatorCtrl', ['$scope', function($scope) {
     $scope.tassianate = function(normal){
-        normal.msg = 'eita';
+		var sentence = "";
+		for(var i = 0; i < normal.msg.length; i++)
+	{
+			var c = normal.msg.charAt(i).toLowerCase();
+			var ic = normal.msg.charCodeAt(i);
+			if((ic >= 33 && ic <= 47) || (ic >= 58 && ic <= 64))
+			{
+				//do smth with sentence
+				arr = sentence.split(" ")
+
+			}
+			sentence += c;
+		}
+        normal.msg = sentence;
     }
 }]);
