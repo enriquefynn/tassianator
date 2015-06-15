11815f5c09bb10120bcdc74a4ef39105952a4c99
Leonardo Alt <leonardoaltt@gmail.com>
2015-06-15 23:56:05 +0200

    ausdhuasdhuasdhauhd

--- tassianator.js
+++ tassianator.js
@@ -1,6 +1,7 @@
 'use strict';
 var App = angular.module('App', []);
 function shuffle(o){for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);return o;}
+function isControlChar(ic) { return ((ic >= 33 && ic <= 47) || (ic >= 58 && ic <= 64)); }
 App.controller('tassianatorCtrl', ['$scope', '$timeout', function($scope, $timeout){
     $scope.waiting = false;
     $scope.tassianate = function(normal){
@@ -15,14 +16,16 @@ App.controller('tassianatorCtrl', ['$scope', '$timeout', function($scope, $timeo
             {
                 var c = normal.msg.charAt(i).toLowerCase();
                 var ic = normal.msg.charCodeAt(i);
-                if((ic >= 33 && ic <= 47) || (ic >= 58 && ic <= 64))
+                if(isControlChar(ic))
                 {
                     //do smth with sentence
 					if(sentence.substring(0, 1) == ' ') sentence = sentence.substring(1);
                     var arr = sentence.split(" ");
-					arr = shuffle(arr);
-					if(w > 0) text += " ";
-					text += arr.join(" ") + c;
+					if(arr.length > 1) arr = shuffle(arr);
+					if(arr.length == 1 && arr[0] == '') sentence = c;
+					else sentence = arr.join(" ") + c;
+					if(w > 0 && sentence.length > 0 && !isControlChar(sentence.charCodeAt(0))) text += " ";
+					text += sentence;
 					sentence = "";
 					w++;
 					continue;
