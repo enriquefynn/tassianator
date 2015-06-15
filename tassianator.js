feb57138a3761557ae1b266a1f4544ea021a551e
Leonardo Alt <leonardoaltt@gmail.com>
2015-06-16 00:05:34 +0200

    ausdhuasdhuasdhauhd

--- tassianator.js
+++ tassianator.js
@@ -2,6 +2,7 @@
 var App = angular.module('App', []);
 function shuffle(o){for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);return o;}
 function isControlChar(ic) { return ((ic >= 33 && ic <= 47) || (ic >= 58 && ic <= 64)); }
+function getRandomInt(min, max) { return Math.floor(Math.random() * (max - min)) + min; }
 App.controller('tassianatorCtrl', ['$scope', '$timeout', function($scope, $timeout){
     $scope.waiting = false;
     $scope.tassianate = function(normal){
@@ -21,6 +22,12 @@ App.controller('tassianatorCtrl', ['$scope', '$timeout', function($scope, $timeo
                     //do smth with sentence
 					if(sentence.substring(0, 1) == ' ') sentence = sentence.substring(1);
                     var arr = sentence.split(" ");
+					var idxarr = [];
+					for(var j = arr.length - 1; j >= 0; j--)
+						if(arr[j].length <= 2 && getRandomInt(0, 10) < 5)
+							idxarr.push(j)
+					for(var j = 0; j < idxarr.length; j++)
+						arr.splice(idxarr[j], 1)
 					if(arr.length > 1) arr = shuffle(arr);
 					if(arr.length == 1 && arr[0] == '') sentence = c;
 					else sentence = arr.join(" ") + c;
