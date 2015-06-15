bc006b5e8c49bec58e33e1ab3da9c31e5018fd94
Fynn <enriquefynn@gmail.com>
2015-06-15 17:45:30 -0300

    kkkkkkkkkk

--- tassianator.js
+++ tassianator.js
@@ -1,7 +1,12 @@
 'use strict';
 var App = angular.module('App', []);
-App.controller('tassianatorCtrl', ['$scope', function($scope) {
+App.controller('tassianatorCtrl', ['$scope', '$timeout', function($scope, $timeout){
+    $scope.waiting = false;
     $scope.tassianate = function(normal){
-        normal.msg = 'eita';
+        $scope.waiting = true;
+        $timeout(function(){
+            $scope.waiting = false;
+            normal.msg = 'eita';
+        }, Math.random()*2000+1000);
     }
 }]);
