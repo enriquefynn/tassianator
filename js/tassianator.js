1f586406b045ebd3cd335fcdbca08007da7a9289
Leonardo Alt <leonardoaltt@gmail.com>
2015-06-16 01:01:13 +0200

    aee

--- js/tassianator.js
+++ js/tassianator.js
@@ -3,6 +3,44 @@ var App = angular.module('App', []);
 function shuffle(o){for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);return o;}
 function isControlChar(ic) { return ((ic >= 33 && ic <= 47) || (ic >= 58 && ic <= 64)); }
 function getRandomInt(min, max) { return Math.floor(Math.random() * (max - min)) + min; }
+function isVowel(c) { return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'); }
+function removeVowels(msg)
+{
+	var newstr = "";
+	for(var i = 0; i < msg.length; i++)
+		if(!isVowel(msg.charAt(i)) || getRandomInt(0, 10) >= 1) //10% de chance de remover vogal
+			newstr += msg.charAt(i);
+	return newstr;
+}
+function removeDoubles(msg)
+{
+	var last = msg.charAt(0);
+	var newstr = "" + last;
+	for(var i = 1; i < msg.length; i++)
+	{
+		var c = msg.charAt(i);
+		if(c != last || getRandomInt(0, 10) >= 2) newstr += c; //20% de chance de remover letra dupla
+		last = c;
+	}
+	return newstr;
+}
+function handleSentence(sentence, c, w)
+{
+	var newstr = "" + sentence;
+	if(sentence.substring(0, 1) == ' ') newstr = sentence.substring(1);
+	var arr = newstr.split(" ");
+	var idxarr = [];
+	for(var j = arr.length - 1; j >= 0; j--)
+		if(arr[j].length <= 2 && getRandomInt(0, 10) < 5) //50% de chance de remover palavras de tamanho <= 2
+			idxarr.push(j)
+	for(var j = 0; j < idxarr.length; j++)
+		arr.splice(idxarr[j], 1)
+	if(arr.length > 1) arr = shuffle(arr);
+	if(arr.length == 1 && arr[0] == '') newstr = c;
+	else newstr = arr.join(" ") + c;
+	if(w > 0 && newstr.length > 0 && !isControlChar(newstr.charCodeAt(0))) newstr = " " + newstr;
+	return newstr;
+}
 App.controller('tassianatorCtrl', ['$scope', '$timeout', function($scope, $timeout){
     $scope.waiting = false;
     $scope.tassianate = function(normal){
@@ -15,35 +53,23 @@ App.controller('tassianatorCtrl', ['$scope', '$timeout', function($scope, $timeo
             var sentence = "";
 			var text = "";
 			var w = 0;
+			normal.msg = removeVowels(normal.msg);
+			normal.msg = removeDoubles(normal.msg);
+			
             for(var i = 0; i < normal.msg.length; i++)
             {
                 var c = normal.msg.charAt(i).toLowerCase();
                 var ic = normal.msg.charCodeAt(i);
                 if(isControlChar(ic))
                 {
-                    //do smth with sentence
-					if(sentence.substring(0, 1) == ' ') sentence = sentence.substring(1);
-                    var arr = sentence.split(" ");
-					var idxarr = [];
-					for(var j = arr.length - 1; j >= 0; j--)
-						if(arr[j].length <= 2 && getRandomInt(0, 10) < 5)
-							idxarr.push(j)
-					for(var j = 0; j < idxarr.length; j++)
-						arr.splice(idxarr[j], 1)
-					if(arr.length > 1) arr = shuffle(arr);
-					if(arr.length == 1 && arr[0] == '') sentence = c;
-					else sentence = arr.join(" ") + c;
-					if(w > 0 && sentence.length > 0 && !isControlChar(sentence.charCodeAt(0))) text += " ";
-					text += sentence;
+					text += handleSentence(sentence, c, w);
 					sentence = "";
 					w++;
 					continue;
                 }
                 sentence += c;
             }
-			var arr = sentence.split(" ");
-			arr = shuffle(arr);
-			text += arr.join(" ");
+			text += handleSentence(sentence, '', w);
             normal.msg = text;
         }, Math.random()*2000+1000);
     }
