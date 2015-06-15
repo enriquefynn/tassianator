b0ab0e7e21940537e89833d05fc0e2ea44b96ee8
Leonardo Alt <leonardoaltt@gmail.com>
2015-06-15 23:45:32 +0200

    ausdhuasdhuasdhauhd

--- tassianator.js
+++ tassianator.js
@@ -1,12 +1,16 @@
 'use strict';
 var App = angular.module('App', []);
+function shuffle(o){for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);return o;}
 App.controller('tassianatorCtrl', ['$scope', '$timeout', function($scope, $timeout){
     $scope.waiting = false;
     $scope.tassianate = function(normal){
         $scope.waiting = true;
         $timeout(function(){
             $scope.waiting = false;
+			
             var sentence = "";
+			var text = "";
+			var w = 0;
             for(var i = 0; i < normal.msg.length; i++)
             {
                 var c = normal.msg.charAt(i).toLowerCase();
@@ -14,11 +18,21 @@ App.controller('tassianatorCtrl', ['$scope', '$timeout', function($scope, $timeo
                 if((ic >= 33 && ic <= 47) || (ic >= 58 && ic <= 64))
                 {
                     //do smth with sentence
-                    arr = sentence.split(" ");
+					if(sentence.substring(0, 1) == ' ') sentence = sentence.substring(1);
+                    var arr = sentence.split(" ");
+					arr = shuffle(arr);
+					if(w > 0) text += " ";
+					text += arr.join(" ") + c;
+					sentence = "";
+					w++;
+					continue;
                 }
                 sentence += c;
             }
-            normal.msg = sentence;
+			var arr = sentence.split(" ");
+			arr = shuffle(arr);
+			text += arr.join(" ");
+            normal.msg = text;
         }, Math.random()*2000+1000);
     }
 }]);
